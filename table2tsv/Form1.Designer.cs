2d797dcf9b12d89792844545f454a1158b2e40dc
szkmiyabi <led.centipede@gmail.com>
2019-08-21 03:06:11 +0900

    アプリ全機能を実装

--- table2tsv/Form1.Designer.cs
+++ table2tsv/Form1.Designer.cs
@@ -35,8 +35,18 @@
             this.domStructButton = new System.Windows.Forms.Button();
             this.saveTSVButton = new System.Windows.Forms.Button();
             this.domResetButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.doTagStripCheck = new System.Windows.Forms.CheckBox();
+            this.aTagHrefCheck = new System.Windows.Forms.CheckBox();
+            this.doBrReplaceCheck = new System.Windows.Forms.CheckBox();
+            this.ampCodeStripCheck = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.rootUrlText = new System.Windows.Forms.TextBox();
+            this.doAddRootPrefixCheck = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -45,24 +55,30 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.inputText, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.browserControl, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 0, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.81638F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.18362F));
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.44304F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.55696F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(577, 439);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(577, 483);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // inputText
             // 
             this.inputText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inputText.Location = new System.Drawing.Point(3, 236);
+            this.inputText.Location = new System.Drawing.Point(3, 217);
+            this.inputText.MaxLength = 0;
             this.inputText.Multiline = true;
             this.inputText.Name = "inputText";
-            this.inputText.Size = new System.Drawing.Size(571, 164);
+            this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputText.Size = new System.Drawing.Size(571, 166);
             this.inputText.TabIndex = 0;
             // 
             // browserControl
@@ -71,7 +87,7 @@
             this.browserControl.Location = new System.Drawing.Point(3, 3);
             this.browserControl.MinimumSize = new System.Drawing.Size(20, 20);
             this.browserControl.Name = "browserControl";
-            this.browserControl.Size = new System.Drawing.Size(571, 227);
+            this.browserControl.Size = new System.Drawing.Size(571, 208);
             this.browserControl.TabIndex = 1;
             // 
             // flowLayoutPanel1
@@ -80,7 +96,7 @@
             this.flowLayoutPanel1.Controls.Add(this.saveTSVButton);
             this.flowLayoutPanel1.Controls.Add(this.domResetButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(169, 406);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(169, 450);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(405, 30);
             this.flowLayoutPanel1.TabIndex = 2;
@@ -93,6 +109,7 @@
             this.domStructButton.TabIndex = 0;
             this.domStructButton.Text = "DOM構築";
             this.domStructButton.UseVisualStyleBackColor = true;
+            this.domStructButton.Click += new System.EventHandler(this.domStructButton_Click);
             // 
             // saveTSVButton
             // 
@@ -102,6 +119,7 @@
             this.saveTSVButton.TabIndex = 1;
             this.saveTSVButton.Text = "TSV保存";
             this.saveTSVButton.UseVisualStyleBackColor = true;
+            this.saveTSVButton.Click += new System.EventHandler(this.saveTSVButton_Click);
             // 
             // domResetButton
             // 
@@ -111,18 +129,104 @@
             this.domResetButton.TabIndex = 2;
             this.domResetButton.Text = "リセット";
             this.domResetButton.UseVisualStyleBackColor = true;
+            this.domResetButton.Click += new System.EventHandler(this.domResetButton_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.ampCodeStripCheck);
+            this.flowLayoutPanel2.Controls.Add(this.doTagStripCheck);
+            this.flowLayoutPanel2.Controls.Add(this.doBrReplaceCheck);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(168, 389);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(406, 22);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // doTagStripCheck
+            // 
+            this.doTagStripCheck.AutoSize = true;
+            this.doTagStripCheck.Location = new System.Drawing.Point(117, 3);
+            this.doTagStripCheck.Name = "doTagStripCheck";
+            this.doTagStripCheck.Size = new System.Drawing.Size(97, 16);
+            this.doTagStripCheck.TabIndex = 0;
+            this.doTagStripCheck.Text = "セル内タグ除去";
+            this.doTagStripCheck.UseVisualStyleBackColor = true;
+            // 
+            // aTagHrefCheck
+            // 
+            this.aTagHrefCheck.AutoSize = true;
+            this.aTagHrefCheck.Location = new System.Drawing.Point(3, 6);
+            this.aTagHrefCheck.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.aTagHrefCheck.Name = "aTagHrefCheck";
+            this.aTagHrefCheck.Size = new System.Drawing.Size(127, 16);
+            this.aTagHrefCheck.TabIndex = 1;
+            this.aTagHrefCheck.Text = "aタグの場合URL抽出";
+            this.aTagHrefCheck.UseVisualStyleBackColor = true;
+            // 
+            // doBrReplaceCheck
+            // 
+            this.doBrReplaceCheck.AutoSize = true;
+            this.doBrReplaceCheck.Location = new System.Drawing.Point(220, 3);
+            this.doBrReplaceCheck.Name = "doBrReplaceCheck";
+            this.doBrReplaceCheck.Size = new System.Drawing.Size(105, 16);
+            this.doBrReplaceCheck.TabIndex = 2;
+            this.doBrReplaceCheck.Text = "brタグを｜に変換";
+            this.doBrReplaceCheck.UseVisualStyleBackColor = true;
+            // 
+            // ampCodeStripCheck
+            // 
+            this.ampCodeStripCheck.AutoSize = true;
+            this.ampCodeStripCheck.Location = new System.Drawing.Point(3, 3);
+            this.ampCodeStripCheck.Name = "ampCodeStripCheck";
+            this.ampCodeStripCheck.Size = new System.Drawing.Size(108, 16);
+            this.ampCodeStripCheck.TabIndex = 3;
+            this.ampCodeStripCheck.Text = "＆実体参照除去";
+            this.ampCodeStripCheck.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.aTagHrefCheck);
+            this.flowLayoutPanel3.Controls.Add(this.doAddRootPrefixCheck);
+            this.flowLayoutPanel3.Controls.Add(this.rootUrlText);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(30, 417);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(544, 27);
+            this.flowLayoutPanel3.TabIndex = 4;
+            // 
+            // rootUrlText
+            // 
+            this.rootUrlText.Location = new System.Drawing.Point(240, 3);
+            this.rootUrlText.Name = "rootUrlText";
+            this.rootUrlText.Size = new System.Drawing.Size(288, 19);
+            this.rootUrlText.TabIndex = 0;
+            // 
+            // doAddRootPrefixCheck
+            // 
+            this.doAddRootPrefixCheck.AutoSize = true;
+            this.doAddRootPrefixCheck.Location = new System.Drawing.Point(136, 6);
+            this.doAddRootPrefixCheck.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.doAddRootPrefixCheck.Name = "doAddRootPrefixCheck";
+            this.doAddRootPrefixCheck.Size = new System.Drawing.Size(98, 16);
+            this.doAddRootPrefixCheck.TabIndex = 2;
+            this.doAddRootPrefixCheck.Text = "ルートURL付与";
+            this.doAddRootPrefixCheck.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 439);
+            this.ClientSize = new System.Drawing.Size(577, 483);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "table2csv";
+            this.Text = "table2tsv";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -136,6 +240,14 @@
         private System.Windows.Forms.Button domStructButton;
         private System.Windows.Forms.Button saveTSVButton;
         private System.Windows.Forms.Button domResetButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.CheckBox doTagStripCheck;
+        private System.Windows.Forms.CheckBox aTagHrefCheck;
+        private System.Windows.Forms.CheckBox doBrReplaceCheck;
+        private System.Windows.Forms.CheckBox ampCodeStripCheck;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.CheckBox doAddRootPrefixCheck;
+        private System.Windows.Forms.TextBox rootUrlText;
     }
 }
 
--- table2tsv/Form1.cs
+++ table2tsv/Form1.cs
@@ -3,17 +3,183 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace table2tsv
 {
     public partial class Form1 : Form
     {
+
+        private string prefix = "<!doctype html><html lang='ja'><head><meta charset='utf-8'></head><body>";
+        private string sufix = "</body></html>";
+
+        //コンストラクタ
         public Form1()
         {
             InitializeComponent();
+            preview_default();
+
+            //デフォルトでチェックON
+            ampCodeStripCheck.Checked = true;
+            doTagStripCheck.Checked = true;
+            aTagHrefCheck.Checked = true;
+            doBrReplaceCheck.Checked = true;
+        }
+
+        //デフォルト表示に戻す
+        private void preview_default()
+        {
+            browserControl.DocumentText = prefix + "<p style='margin-top:30px;text-align:center;'>ここにプレビューが表示される</p>" + sufix;
+        }
+
+        //TSVファイル保存先を取得
+        private string _get_tsv_save_path()
+        {
+            string path = "";
+            SaveFileDialog fda = new SaveFileDialog();
+            fda.Filter = "TSVファイル(*.txt)|*.txt";
+            fda.Title = "名前を付けて保存";
+            if (fda.ShowDialog() == DialogResult.OK)
+            {
+                path = fda.FileName;
+            }
+            return path;
+        }
+
+        //TSV行データの生成
+        private string _edit_row_tsv(List<object> row)
+        {
+            string row_str = "";
+            for (int j = 0; j < row.Count; j++)
+            {
+                row_str += (string)row[j];
+                if (j < (row.Count - 1)) row_str += "\t";
+            }
+            row_str += "\r\n";
+            return row_str;
+        }
+
+        //テキスト整形
+        private string _text_clean(string str)
+        {
+            if(ampCodeStripCheck.Checked == true) {
+                try { str = Regex.Replace(str, @"&nbsp;", ""); } catch (Exception ex) { }
+                try { str = Regex.Replace(str, @"&amp;", "&"); } catch (Exception ex) { }
+            }
+            if (doBrReplaceCheck.Checked == true) {
+                try { str = Regex.Replace(str, @"<br>", "｜", RegexOptions.IgnoreCase); } catch (Exception ex) { }
+            }
+
+            if (doTagStripCheck.Checked == true) {
+                try { str = Regex.Replace(str, @"<\/*.+?>", ""); } catch (Exception ex) { }
+            }
+            return str;
+        }
+
+        //aタグ判定
+        private Boolean _is_a_tag(string str)
+        {
+            if (str == null || str == "") return false;
+            Regex pt = new Regex(@"</*a.+?>", RegexOptions.IgnoreCase);
+            if (pt.IsMatch(str)) return true;
+            else return false;
+        }
+
+        //href抽出
+        private string _get_href_attr(string str)
+        {
+            string ret = "";
+            Regex pt = new Regex(@"(href="")(.+?)("")", RegexOptions.IgnoreCase);
+            if (pt.IsMatch(str))
+            {
+                MatchCollection mc = pt.Matches(str);
+                Match mt = mc[0];
+                ret = mt.Groups[2].Value;
+            }
+            return ret;
+        }
+
+
+        //tableタグをパースする
+        private List<object> get_table_data_for_list()
+        {
+            List<object> arr = new List<object>();
+            HtmlDocument d = browserControl.Document;
+            HtmlElementCollection tbls = d.GetElementsByTagName("table");
+            foreach(HtmlElement tbl in tbls)
+            {
+                HtmlElementCollection trs = tbl.GetElementsByTagName("tr");
+                int i = 0;
+                foreach (HtmlElement tr in trs)
+                {
+                    if (i > 0)
+                    {
+                        List<object> cols = new List<object>();
+                        HtmlElementCollection tds = tr.GetElementsByTagName("td");
+                        foreach (HtmlElement td in tds)
+                        {
+                            string td_val = (string)td.InnerHtml;
+                            string cl_td_val = _text_clean(td_val);
+                            cols.Add(cl_td_val);
+                            if (_is_a_tag(td_val) == true && aTagHrefCheck.Checked == true)
+                            {
+                                string href = _get_href_attr(td_val);
+                                if(doAddRootPrefixCheck.Checked == true && rootUrlText.Text != "")
+                                {
+                                    href = rootUrlText.Text + href;
+                                }
+                                cols.Add(href);
+                            }
+                        }
+                        arr.Add(cols);
+                    }
+                    i++;
+                }
+            }
+            return arr;
+        }
+
+
+        //DOM構築
+        private void domStructButton_Click(object sender, EventArgs e)
+        {
+            if (inputText.Text == "") return;
+            string body = inputText.Text;
+            browserControl.DocumentText = prefix + body + sufix;
+        }
+
+        //TSV保存
+        private void saveTSVButton_Click(object sender, EventArgs e)
+        {
+            string save_path = _get_tsv_save_path();
+
+            //BOM無し設定
+            System.Text.Encoding enc = new System.Text.UTF8Encoding(false);
+            StreamWriter sw = new StreamWriter(save_path, false, enc);
+
+            string body = "";
+            List<object> arr = get_table_data_for_list();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                List<object> row =(List<object>)arr[i];
+                string row_str = _edit_row_tsv(row);
+                sw.Write(row_str);
+            }
+
+            sw.Close();
+            MessageBox.Show("処理が完了しました!");
+
+        }
+
+        //リセット
+        private void domResetButton_Click(object sender, EventArgs e)
+        {
+            inputText.Clear();
+            preview_default();
         }
     }
 }
